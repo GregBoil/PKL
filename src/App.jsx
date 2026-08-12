3c0297e64fef48d43cda49144761d0a8e0666eb8
GregoB <90229761+GregBoil@users.noreply.github.com>
2026-08-12 23:09:08 +0800

    Retour haut et changement font eyebrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import { Footer, Header } from './components/Layout'
 import Contact from './pages/Contact'
 import Financement2 from './pages/Financement_2'
 import Formations from './pages/Formations'
 import Home from './pages/Home'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' })
+  }, [pathname])
+
+  return null
+}
+
 export default function App() {
   return (
     <>
+      <ScrollToTop />
       <Header />
 
       <main>
